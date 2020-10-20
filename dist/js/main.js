eba48cf71104c8bd09313c8751177bf87af8b7e0
steve <steve@gmail.com>
2020-10-20 17:18:45 +0800

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,17 +13,17 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   if(!showMenu){
     menuBtn.classList.add('close');
-    menu.classList.add('close');
-    menuNav.classList.add('close');
-    menuBranding.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));  
     // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
-    menu.classList.remove('close');
-    menuNav.classList.remove('close');
-    menuBranding.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));  
     // Set Menu State
     showMenu = false;
